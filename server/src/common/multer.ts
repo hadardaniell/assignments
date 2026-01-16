448c5ca14eaeb92ed85dac3571abbcea665cb77e
AD22113 <ad80808080@gmail.com>
2026-01-16 23:04:27 +0200

    More Multer changes

--- server/src/common/multer.ts
+++ server/src/common/multer.ts
@@ -3,10 +3,9 @@ import path from 'path';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // כאן נשמרים הקבצים
+        cb(null, 'uploads/');
     },
     filename: (req, file, cb) => {
-        // יוצר שם ייחודי: timestamp + מספר רנדומלי + סיומת מקורית
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, uniqueSuffix + path.extname(file.originalname));
     }
