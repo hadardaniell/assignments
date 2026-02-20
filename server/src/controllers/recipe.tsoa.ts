2827e4ce0e4c92e7a26dc231e07cfb5c2ee9bf8b
hadardaniell <hadar808080@gmail.com>
2026-02-20 15:29:58 +0200

    fix get recipes with user likes

--- server/src/controllers/recipe.tsoa.ts
+++ server/src/controllers/recipe.tsoa.ts
@@ -75,7 +75,7 @@ export class RecipeController extends Controller {
         @Query() limit?: number
     ): Promise<RecipeDTO[]> {
         try {
-            const userId = req.userId;
+            const userId = req.user?.id;
             const filter: RecipeFilterDTO = { recipeBookId, status, difficulty, search, skip, limit };
             return await this.service.listRecipes(filter, userId);
         } catch (err: any) {
